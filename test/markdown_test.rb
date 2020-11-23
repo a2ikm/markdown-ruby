e723c95e8412f2ce021689a5b2cdbec32a8548ee
Masato Ikeda <masato.ikeda@gmail.com>
2020-11-23 16:59:16 +0900

    Reduce test case

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -3,14 +3,8 @@ require "test_helper"
 class MarkdownTest < Minitest::Test
   def test_paragraph
     assert_markdown "hello"
-  end
-
-  def test_paragraph_with_newline
     assert_markdown "hello\nworld"
     assert_markdown "hello\nworld\nagain"
-  end
-
-  def test_paragraphs
     assert_markdown "hello\n\nworld"
     assert_markdown "hello\n\nworld\n\nagain"
   end
