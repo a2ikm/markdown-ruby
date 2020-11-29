78595f70835eff9772b7a8332da2f3f991d7b71f
Masato Ikeda <masato.ikeda@gmail.com>
2020-11-29 16:00:56 +0900

    Remove regexps from rendering

--- lib/markdown.rb
+++ lib/markdown.rb
@@ -7,18 +7,18 @@ module Markdown
     markdown.each_line do |line|
       if line == "\n"
         unless buffer.empty?
-          blocks << buffer.dup
+          blocks << [:p, nil, buffer.dup]
           buffer.clear
         end
         next
       end
 
-      if line.match?(/\A\#{1,6}\s+.+/)
+      if m = line.match(/\A(\#{1,6})\s+(.+)/)
         unless buffer.empty?
-          blocks << buffer.dup
+          blocks << [:p, nil, buffer.dup]
           buffer.clear
         end
-        buffer << line
+        blocks << [:h, { level: m[1].length }, [m[2]]]
         next
       end
 
@@ -26,17 +26,19 @@ module Markdown
     end
 
     unless buffer.empty?
-      blocks << buffer.dup
+      blocks << [:p, nil, buffer.dup]
+      buffer.clear
     end
 
     out = StringIO.new
 
-    blocks.each do |block|
-      case block.first
-      when /\A(\#{1,6})\s+(.+)/
-        out.write "<h#{$1.length}>#{$2}</h#{$1.length}>\n"
-      else
-        out.write "<p>#{block.join.chomp}</p>\n"
+    blocks.each do |(type, opts, lines)|
+      case type
+      when :h
+        tag = "h#{opts[:level]}"
+        out.write "<#{tag}>#{lines.join.chomp}</#{tag}>\n"
+      when :p
+        out.write "<p>#{lines.join.chomp}</p>\n"
       end
     end
 
